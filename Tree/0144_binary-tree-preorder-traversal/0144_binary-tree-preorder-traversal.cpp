9c2c4f5aab4714ad07746c32fa982f00ce741070
Subhojit <ksubhojit20@gmail.com>
2026-03-28 23:20:26 +0530

    Add Easy solution: 144. Binary Tree Preorder Traversal [cpp]

---
+++ Tree/0144_binary-tree-preorder-traversal/0144_binary-tree-preorder-traversal.cpp
@@ -0,0 +1,34 @@
+/*
+ * Problem  : 144. Binary Tree Preorder Traversal
+ * Difficulty: Easy
+ * Topics   : Stack, Tree, Depth-First Search, Binary Tree
+ * URL      : https://leetcode.com/problems/binary-tree-preorder-traversal/
+ */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void solve(TreeNode* root, vector<int> &ans){
+        if(root==NULL){
+            return;
+        }
+        ans.push_back(root->val);
+        solve(root->left,ans);
+        solve(root->right,ans);
+    }
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        solve(root,ans);
+        return ans;
+    }
+};
