e22ac972c6f31eb1cabea2ef5ce4d12e828ace62
Subhojit <ksubhojit20@gmail.com>
2026-03-28 23:15:27 +0530

    Add Easy solution: 20. Valid Parentheses [cpp]

---
+++ String/0020_valid-parentheses/0020_valid-parentheses.cpp
@@ -0,0 +1,36 @@
+/*
+ * Problem  : 20. Valid Parentheses
+ * Difficulty: Easy
+ * Topics   : String, Stack
+ * URL      : https://leetcode.com/problems/valid-parentheses/
+ */
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(int i=0;i<s.length();i++){
+            char ch=s[i];
+            if(ch=='(' || ch=='{' || ch=='['){
+                st.push(ch);
+            }else{
+                if(st.empty()){
+                    return false;
+                }
+                if(ch==']' && st.top()=='['){
+                    st.pop();
+                }else if(ch==')' && st.top()=='('){
+                    st.pop();
+                }else if(ch=='}' && st.top()=='{'){
+                    st.pop();
+                }else{
+                    return false;
+                }
+            }
+        }
+        if(st.empty()){
+            return true;
+        }
+        return false;
+    }
+};
