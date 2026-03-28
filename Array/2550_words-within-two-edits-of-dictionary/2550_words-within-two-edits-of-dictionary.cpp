b614595ec71af1b61e62d1d13cc3d6f3e045421b
Subhojit <ksubhojit20@gmail.com>
2026-03-28 23:19:36 +0530

    Add Medium solution: 2550. Words Within Two Edits of Dictionary [cpp]

---
+++ Array/2550_words-within-two-edits-of-dictionary/2550_words-within-two-edits-of-dictionary.cpp
@@ -0,0 +1,34 @@
+/*
+ * Problem  : 2550. Words Within Two Edits of Dictionary
+ * Difficulty: Medium
+ * Topics   : Array, String, Trie
+ * URL      : https://leetcode.com/problems/words-within-two-edits-of-dictionary/
+ */
+
+class Solution {
+private:
+    int fun(string &a, string &b){
+        int k = 0;
+        for(int i=0; i<a.size(); i++){
+            if(a[i]!=b[i])  k++;
+        }
+        return k;
+    }
+public:
+    vector<string> twoEditWords(vector<string>& q, vector<string>& d) {
+        int n = q.size(), m = d.size();
+        vector<string> ans;
+        for(int i=0; i<n; i++){
+            string a = q[i];
+            for(int j=0; j<m; j++){
+                string b = d[j];
+                int check = fun(a, b);
+                if(check<=2){
+                    ans.push_back(a);
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+};
