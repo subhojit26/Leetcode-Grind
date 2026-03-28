020f710c66c89bf713462a1a2b4dfc0b8ee6e776
Subhojit <ksubhojit20@gmail.com>
2026-03-28 23:19:31 +0530

    Add Medium solution: 53. Maximum Subarray [cpp]

---
+++ Array/0053_maximum-subarray/0053_maximum-subarray.cpp
@@ -0,0 +1,19 @@
+/*
+ * Problem  : 53. Maximum Subarray
+ * Difficulty: Medium
+ * Topics   : Array, Divide and Conquer, Dynamic Programming
+ * URL      : https://leetcode.com/problems/maximum-subarray/
+ */
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int sum=0;
+        int ans=INT_MIN;
+        for(int i=0;i<nums.size();i++){
+            sum=max(nums[i],sum+nums[i]);
+            ans=max(sum,ans);
+        }
+        return ans;
+    }
+};
