f891b1fcdd53f5d4ce8547b835c5aab9e0714f9e
Subhojit <ksubhojit20@gmail.com>
2026-03-28 23:21:22 +0530

    Add Medium solution: 215. Kth Largest Element in an Array [cpp]

---
+++ Array/0215_kth-largest-element-in-an-array/0215_kth-largest-element-in-an-array.cpp
@@ -0,0 +1,21 @@
+/*
+ * Problem  : 215. Kth Largest Element in an Array
+ * Difficulty: Medium
+ * Topics   : Array, Divide and Conquer, Sorting, Heap (Priority Queue), Quickselect
+ * URL      : https://leetcode.com/problems/kth-largest-element-in-an-array/
+ */
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int, vector<int>, greater<int>> pq;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            pq.push(nums[i]);
+        }
+        while(pq.size()>k){
+            pq.pop();
+        }
+        return pq.top();
+    }
+};
