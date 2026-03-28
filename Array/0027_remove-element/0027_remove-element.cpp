1e2c02dcda0a72143c59bd0c4270f634650e72aa
Subhojit <ksubhojit20@gmail.com>
2026-03-28 23:21:15 +0530

    Add Easy solution: 27. Remove Element [cpp]

---
+++ Array/0027_remove-element/0027_remove-element.cpp
@@ -0,0 +1,20 @@
+/*
+ * Problem  : 27. Remove Element
+ * Difficulty: Easy
+ * Topics   : Array, Two Pointers
+ * URL      : https://leetcode.com/problems/remove-element/
+ */
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int index = 0;
+        for(int i = 0; i< nums.size(); i++){
+            if(nums[i] != val){
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+        return index;
+    }
+};
