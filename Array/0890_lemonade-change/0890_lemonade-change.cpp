dfdb6b56f45d38fa5eb1a5a1f3fc1e5b5065f928
Subhojit <ksubhojit20@gmail.com>
2026-03-28 23:24:22 +0530

    Add Easy solution: 890. Lemonade Change [cpp]

---
+++ Array/0890_lemonade-change/0890_lemonade-change.cpp
@@ -0,0 +1,37 @@
+/*
+ * Problem  : 890. Lemonade Change
+ * Difficulty: Easy
+ * Topics   : Array, Greedy
+ * URL      : https://leetcode.com/problems/lemonade-change/
+ */
+
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int five_dollars = 0, ten_dollars = 0;
+        
+        for (int x : bills) {
+            if (x == 5) {
+                five_dollars++;
+            } else if (x == 10) {
+                if (five_dollars > 0) {
+                    five_dollars--;
+                    ten_dollars++;
+                } else {
+                    return false;
+                }
+            } else {
+                if (five_dollars > 0 && ten_dollars > 0) {
+                    five_dollars--;
+                    ten_dollars--;
+                } else if (five_dollars > 2) {
+                    five_dollars -= 3;
+                } else {
+                    return false;
+                }
+            }
+        }
+        
+        return true;
+    }
+};
