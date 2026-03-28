f557f848d12e64126486e29c88e2283602e1e5fb
Subhojit <ksubhojit20@gmail.com>
2026-03-28 23:15:34 +0530

    Add Medium solution: 134. Gas Station [cpp]

---
+++ Array/0134_gas-station/0134_gas-station.cpp
@@ -0,0 +1,26 @@
+/*
+ * Problem  : 134. Gas Station
+ * Difficulty: Medium
+ * Topics   : Array, Greedy
+ * URL      : https://leetcode.com/problems/gas-station/
+ */
+
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int n=gas.size();
+        int surplus=0;
+        int total_surplus=0;
+        int start=0;
+        for(int i=0;i<n;i++){
+            total_surplus+=gas[i]-cost[i];
+            surplus+=gas[i]-cost[i];
+            if(surplus<0){
+                surplus=0;
+                start=i+1;
+            }
+
+        }
+        return (total_surplus<0)?-1:start;
+    }
+};
