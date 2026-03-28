35aac0d567698f6c66c4a10ea5d574a57b591b41
Subhojit <ksubhojit20@gmail.com>
2026-03-28 23:17:52 +0530

    Add Medium solution: 1250. Longest Common Subsequence [cpp]

---
+++ String/1250_longest-common-subsequence/1250_longest-common-subsequence.cpp
@@ -0,0 +1,30 @@
+/*
+ * Problem  : 1250. Longest Common Subsequence
+ * Difficulty: Medium
+ * Topics   : String, Dynamic Programming
+ * URL      : https://leetcode.com/problems/longest-common-subsequence/
+ */
+
+class Solution {
+public:
+    int solver(int i, int j, string &s1, string &s2, vector<vector<int>> &dp){
+        if(i<0 || j<0){
+            return 0;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+
+        if(s1[i]==s2[j]){
+            return dp[i][j]= 1+solver(i-1,j-1,s1,s2,dp);
+        }
+        return dp[i][j]= max(solver(i-1,j,s1,s2,dp),solver(i,j-1,s1,s2,dp));
+
+    }
+    int longestCommonSubsequence(string text1, string text2) {
+        int n=text1.length();
+        int m=text2.length();
+        vector<vector<int>> dp(n+1, vector<int> (m+1,-1));
+        return solver(n-1,m-1,text1,text2,dp);
+    }
+};
