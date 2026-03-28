3cb3729c7107ffc21572c186fd4d83e412ecab59
Subhojit <ksubhojit20@gmail.com>
2026-03-28 23:19:22 +0530

    Add Hard solution: 188. Best Time to Buy and Sell Stock IV [cpp]

---
+++ Array/0188_best-time-to-buy-and-sell-stock-iv/0188_best-time-to-buy-and-sell-stock-iv.cpp
@@ -0,0 +1,31 @@
+/*
+ * Problem  : 188. Best Time to Buy and Sell Stock IV
+ * Difficulty: Hard
+ * Topics   : Array, Dynamic Programming
+ * URL      : https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iv/
+ */
+
+class Solution {
+public:
+    int solver(int ind, int buy, int trans, int n, vector<int> &prices, vector<vector<vector<int>>> &dp){
+        if(ind==n || trans==0){
+            return 0;
+        }
+
+        if(dp[ind][buy][trans]!=-1){
+            return dp[ind][buy][trans];
+        }
+
+        if(buy){
+            return dp[ind][buy][trans]=max(-prices[ind]+solver(ind+1,0,trans,n,prices,dp),solver(ind+1,1,trans,n,prices,dp));
+        }
+        return dp[ind][buy][trans]=max(prices[ind]+solver(ind+1,1,trans-1,n,prices,dp),solver(ind+1,0,trans,n,prices,dp));
+
+    }
+    int maxProfit(int k, vector<int>& prices) {
+        int n=prices.size();
+        int ind=0;
+        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int> (k+1,-1)));
+        return solver(ind,1,k,n,prices,dp);
+    }
+};
