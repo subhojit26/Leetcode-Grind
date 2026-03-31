1c0440d2e8bf30adf8aa10312f77ec4bdcb17667
Subhojit <ksubhojit20@gmail.com>
2026-03-31 10:40:45 +0530

    Update Medium solution: 714. Best Time to Buy and Sell Stock with Transaction Fee [cpp]

--- Array/0714_best-time-to-buy-and-sell-stock-with-transaction-fee/0714_best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ Array/0714_best-time-to-buy-and-sell-stock-with-transaction-fee/0714_best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -7,24 +7,29 @@
 
 class Solution {
 public:
-    int solver(int ind, vector<int> &prices, int fee, bool buy){
+    int solver(int ind, vector<int> &prices, int fee, bool buy, vector<vector<int>> &dp){
         if(ind>=prices.size()){
             return 0;
+        }
+        if(dp[ind][buy]!=-1){
+            return dp[ind][buy];
         }
         int profit=0;
 
         if(buy){
-            profit= max(-prices[ind]+solver(ind+1,prices,fee,false),solver(ind+1,prices,fee,true));
+            profit= max(-prices[ind]+solver(ind+1,prices,fee,false,dp),solver(ind+1,prices,fee,true,dp));
             
         }
         if(!buy){
-            profit= max(solver(ind+1,prices,fee,false),prices[ind]+solver(ind+1,prices,fee,true)-fee);
+            profit= max(solver(ind+1,prices,fee,false,dp),prices[ind]+solver(ind+1,prices,fee,true,dp)-fee);
         }
         
-        return profit;
+        return dp[ind][buy]=profit;
     }
     int maxProfit(vector<int>& prices, int fee) {
         bool buy=true;
-        return solver(0,prices,fee,buy);
+        int n=prices.size();
+        vector<vector<int>> dp(n, vector<int>(2, -1));
+        return solver(0,prices,fee,buy,dp);
     }
 };
