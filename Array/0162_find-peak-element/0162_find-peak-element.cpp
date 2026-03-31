2e027286211e6ad9046c1449ee6479aeb59fe7d2
Subhojit <ksubhojit20@gmail.com>
2026-03-31 09:39:27 +0530

    Update Medium solution: 162. Find Peak Element [cpp]

--- Array/0162_find-peak-element/0162_find-peak-element.cpp
+++ Array/0162_find-peak-element/0162_find-peak-element.cpp
@@ -8,22 +8,26 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        if(nums.size() == 1) return 0; 
-        
-        int n = nums.size();
-        
-        if(nums[0] > nums[1]) return 0;
-        if(nums[n-1] > nums[n-2]) return n-1;
-		
-        int start = 1;
-        int end = n-2;
-        
-        while(start <= end) {
-            int mid = start + (end - start)/2;
-            if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]) return mid;
-            else if(nums[mid] < nums[mid-1]) end = mid - 1;
-            else if(nums[mid] < nums[mid+1]) start = mid + 1;
+        int n=nums.size();
+        int low=0;
+        int high=n-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(mid==0 && nums[mid]>nums[mid+1]){
+                return mid;
+            }
+            if(mid==n-1 && nums[mid]>nums[mid-1]){
+                return mid;
+            }
+            if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]){
+                return mid;
+            }
+            else if(nums[mid+1]>nums[mid]){
+                low=mid+1;
+            }else{
+                high=mid-1;
+            }
         }
-        return -1; 
+        return low;
     }
 };
