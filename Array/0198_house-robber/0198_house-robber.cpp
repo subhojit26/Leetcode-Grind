042ab212dcee7e33748bd117f9d6e163c7e3d48d
Subhojit <ksubhojit20@gmail.com>
2026-04-01 22:55:33 +0530

    Update Medium solution: 198. House Robber [cpp]

--- Array/0198_house-robber/0198_house-robber.cpp
+++ Array/0198_house-robber/0198_house-robber.cpp
@@ -7,16 +7,20 @@
 
 class Solution {
 public:
-    int solver(int ind, vector<int> &nums){
+    int solver(int ind, vector<int> &nums,vector<int>&dp){
         if(ind>=nums.size()){
             return 0;
         }
+        if(dp[ind]!=-1){
+            return dp[ind];
+        }
 
-        int take=nums[ind]+solver(ind+2,nums);
-        int notTake=solver(ind+1,nums);
-        return max(take,notTake);
+        int take=nums[ind]+solver(ind+2,nums,dp);
+        int notTake=solver(ind+1,nums,dp);
+        return dp[ind]=max(take,notTake);
     }
     int rob(vector<int>& nums) {
-        return solver(0,nums);
+        vector<int>dp(nums.size(),-1);
+        return solver(0,nums,dp);
     }
 };
