ed59718a29bbcb7d6cad76c67cdbcad7e575aea5
Subhojit <ksubhojit20@gmail.com>
2026-04-02 23:40:28 +0530

    Update Medium solution: 739. Daily Temperatures [cpp]

--- Array/0739_daily-temperatures/0739_daily-temperatures.cpp
+++ Array/0739_daily-temperatures/0739_daily-temperatures.cpp
@@ -10,7 +10,7 @@ public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
         int n = temperatures.size();
         vector<int> ans(n, 0);
-        stack<int> st; // store indices
+        stack<int> st; 
 
         for(int i = n - 1; i >= 0; i--) {
             while(!st.empty() && temperatures[st.top()] <= temperatures[i]) {
