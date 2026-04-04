782e1dd0163e0b71c7efc7dbdf475300a6cbc700
Subhojit <ksubhojit20@gmail.com>
2026-04-04 10:16:27 +0530

    Add Easy solution: 338. Counting Bits [cpp]

---
+++ Dynamic_Programming/0338_counting-bits/0338_counting-bits.cpp
@@ -0,0 +1,22 @@
+/*
+ * Problem  : 338. Counting Bits
+ * Difficulty: Easy
+ * Topics   : Dynamic Programming, Bit Manipulation
+ * URL      : https://leetcode.com/problems/counting-bits/
+ */
+
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> ans(n+1);
+        ans[0] = 0;
+        for(int i=1;i<=n;i++){
+            if(i%2==0) {
+                ans[i]=ans[i/2];
+            }else{
+                ans[i]=ans[i/2]+1;
+            }
+        }
+        return ans;
+    }
+};
