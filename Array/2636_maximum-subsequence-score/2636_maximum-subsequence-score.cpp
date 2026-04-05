7158507fab19d60937a737ac80491646adc06301
Subhojit <ksubhojit20@gmail.com>
2026-04-05 12:37:16 +0530

    Add Medium solution: 2636. Maximum Subsequence Score [cpp]

---
+++ Array/2636_maximum-subsequence-score/2636_maximum-subsequence-score.cpp
@@ -0,0 +1,38 @@
+/*
+ * Problem  : 2636. Maximum Subsequence Score
+ * Difficulty: Medium
+ * Topics   : Array, Greedy, Sorting, Heap (Priority Queue)
+ * URL      : https://leetcode.com/problems/maximum-subsequence-score/
+ */
+
+class Solution {
+public:
+    long long maxScore(vector<int>& nums1, vector<int>& nums2, int k) {
+        vector<pair<int, int>> v;
+        int n=nums1.size();
+        for (int i = 0; i < n; i++) {
+            v.push_back({nums2[i], nums1[i]});
+        }
+
+        sort(v.rbegin(), v.rend()); // sort by nums2 DESC
+
+        priority_queue<int, vector<int>, greater<int>> minHeap;
+
+        long long sum = 0, ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += v[i].second;
+            minHeap.push(v[i].second);
+
+            if (minHeap.size() > k) {
+                sum -= minHeap.top();
+                minHeap.pop();
+            }
+
+            if (minHeap.size() == k) {
+                ans = max(ans, sum * v[i].first);
+            }
+        }
+        return ans;
+    }
+    };
