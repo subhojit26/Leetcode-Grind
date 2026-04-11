6ad9b7df54c7794d88f70d1ff8767f146b733625
Subhojit <ksubhojit20@gmail.com>
2026-04-11 09:43:23 +0530

    Add Easy solution: 969. Number of Recent Calls [cpp]

---
+++ Queue/0969_number-of-recent-calls/0969_number-of-recent-calls.cpp
@@ -0,0 +1,28 @@
+/*
+ * Problem  : 969. Number of Recent Calls
+ * Difficulty: Easy
+ * Topics   : Design, Queue, Data Stream
+ * URL      : https://leetcode.com/problems/number-of-recent-calls/
+ */
+
+class RecentCounter {
+public:
+    queue<int> q; 
+    RecentCounter() {
+        
+    }
+    
+    int ping(int t) {
+        q.push(t);
+        while(t-q.front()>3000){
+            q.pop();
+        }
+        return q.size();
+    }
+};
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter* obj = new RecentCounter();
+ * int param_1 = obj->ping(t);
+ */
