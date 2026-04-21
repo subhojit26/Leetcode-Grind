eb39174672579216328ae6fe88056d42cdb6a0e1
Subhojit <ksubhojit20@gmail.com>
2026-04-21 09:34:58 +0530

    Update Easy solution: 205. Isomorphic Strings [cpp]

--- String/0205_isomorphic-strings/0205_isomorphic-strings.cpp
+++ String/0205_isomorphic-strings/0205_isomorphic-strings.cpp
@@ -8,18 +8,28 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        unordered_map<char, char> mp;
+        unordered_map<char, char> mp1,mp2;
         int l=s.length();
-        int i=0;
+        int i=0,j=0;
         while(i<l){
-            if(mp.find(s[i])!=mp.end()){
-                if(mp[s[i]]!=t[i]){
+            if(mp1.find(s[i])!=mp1.end()){
+                if(mp1[s[i]]!=t[i]){
                     return false;
                 }
             }else{
-                mp[s[i]]=t[i];
+                mp1[s[i]]=t[i];
             }
             i++;
+        }
+        while(j<l){
+            if(mp2.find(t[j])!=mp2.end()){
+                if(mp2[t[j]]!=s[j]){
+                    return false;
+                }
+            }else{
+                mp2[t[j]]=s[j];
+            }
+            j++;
         }
         return true;
     }
