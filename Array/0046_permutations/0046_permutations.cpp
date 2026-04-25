ccef652f896f5ac90c9266f1afacb082739898dc
Subhojit <ksubhojit20@gmail.com>
2026-04-25 09:48:06 +0530

    Update Medium solution: 46. Permutations [cpp]

--- Array/0046_permutations/0046_permutations.cpp
+++ Array/0046_permutations/0046_permutations.cpp
@@ -7,28 +7,30 @@
 
 class Solution {
 public:
-    void solve(vector<int> nums, int freq[], vector<int> temp, vector<vector<int>> &ans){
+    void solver(vector<int> nums,vector<int> freq, vector<int> &temp, vector<vector<int>> & ans){
+        
         if(temp.size()==nums.size()){
             ans.push_back(temp);
             return;
         }
+
         for(int i=0;i<nums.size();i++){
             if(freq[i]==0){
                 temp.push_back(nums[i]);
                 freq[i]=1;
-                solve(nums,freq,temp,ans);
+                solver(nums,freq,temp,ans);
                 freq[i]=0;
                 temp.pop_back();
             }
         }
+
     }
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>> ans;
         vector<int> temp;
-        int freq[nums.size()];
-        for(int i=0;i<nums.size();i++)
-            freq[i] = 0;
-        solve(nums,freq,temp,ans);
+        vector<vector<int>> ans;
+        int n=nums.size();
+        vector<int> freq(n,0);
+        solver(nums,freq, temp, ans);
         return ans;
     }
 };
