780387571dc40d27085da9283ad877c7bc56d9ae
Subhojit <ksubhojit20@gmail.com>
2026-04-25 10:35:29 +0530

    Update Medium solution: 79. Word Search [cpp]

--- Array/0079_word-search/0079_word-search.cpp
+++ Array/0079_word-search/0079_word-search.cpp
@@ -7,28 +7,24 @@
 
 class Solution {
 public:
-    bool dfs(int it, int i, int j, vector<vector<int>> vis, int m, int n, vector<vector<char>> &board, string word,string temp, vector<int> dr, vector<int> dc){
-        if(it>word.length()){
-            return false;
-        }
-        if(word.length()==1 && board[i][j]==word[it]){
+    bool dfs(int it, int i, int j, vector<vector<int>> vis, int m, int n, vector<vector<char>> &board, string word, vector<int> dr, vector<int> dc){
+        if(it==word.length()){
             return true;
         }
 
         vis[i][j]=1;
-        if(temp==word){
-            return true;
-        }
-        cout<<temp<<" ";
+
         for(int z=0;z<4;z++){
             int nr=i+dr[z];
             int nc=j+dc[z];
 
             if(nr>=0 && nr<m && nc>=0 && nc<n && vis[nr][nc]==0 && board[nr][nc]==word[it]){
-                temp=temp+word[it];
-                return dfs(it+1,nr,nc,vis,m,n,board,word,temp,dr,dc);
+                if(dfs(it+1,nr,nc,vis,m,n,board,word,dr,dc)){
+                    return true;
+                }
             }
         }
+        vis[i][j]=0;
         return false;
 
     }
@@ -37,15 +33,16 @@ public:
         vector<int> dc={0,0,-1,1};
         int m=board.size();
         int n=board[0].size();
-
-        vector<vector<int>> vis(m, vector<int>(n,0));
-
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(dfs(0,i,j,vis,m,n,board,word,"",dr,dc)){
-                    return true;
-                }
+                if(board[i][j]==word[0]){
+                vector<vector<int>> vis(m, vector<int>(n,0));
+
+                    if(dfs(1,i,j,vis,m,n,board,word,dr,dc)){
+                        return true;
+                    }
             }
+        }
         }
         return false;
     }
