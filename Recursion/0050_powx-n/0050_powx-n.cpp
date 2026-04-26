252c772791ccb6df282abfd891887ff2a1839eae
Subhojit <ksubhojit20@gmail.com>
2026-04-26 09:59:04 +0530

    Add Medium solution: 50. Pow(x, n) [cpp]

---
+++ Recursion/0050_powx-n/0050_powx-n.cpp
@@ -0,0 +1,29 @@
+/*
+ * Problem  : 50. Pow(x, n)
+ * Difficulty: Medium
+ * Topics   : Math, Recursion
+ * URL      : https://leetcode.com/problems/powx-n/
+ */
+
+class Solution {
+public:
+    double solver(double x, int n){
+        if(n==0){
+            return 1.0;
+        }
+
+        if(n<0){
+            return 1.0/solver(x,-n);
+        }
+
+        if(n%2==0){
+            return solver(x*x,n/2);
+        }else{
+            return x*solver(x,n-1);
+        }
+    }
+    double myPow(double x, int n) {
+        return solver(x, n);
+
+    }
+};
