574e5a38f424b1981cc9d48edea5414b9bf8565d
Subhojit <ksubhojit20@gmail.com>
2026-04-26 09:19:05 +0530

    Update Medium solution: 53. Maximum Subarray [cpp]

--- Array/0053_maximum-subarray/0053_maximum-subarray.cpp
+++ Array/0053_maximum-subarray/0053_maximum-subarray.cpp
@@ -9,11 +9,15 @@ class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
         int sum=0;
-        int ans=INT_MIN;
+        int mx=INT_MIN;
+
         for(int i=0;i<nums.size();i++){
-            sum=max(nums[i],sum+nums[i]);
-            ans=max(sum,ans);
+            sum+=nums[i];
+            if(sum<0){
+                sum=0;
+            }
+            mx=max(mx,sum);
         }
-        return ans;
+        return mx;
     }
 };
