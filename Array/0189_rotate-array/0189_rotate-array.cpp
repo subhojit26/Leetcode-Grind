050ca2883f281449aafc97e07add23cbed6dceed
Subhojit <ksubhojit20@gmail.com>
2026-04-29 10:09:18 +0530

    Update Medium solution: 189. Rotate Array [cpp]

--- Array/0189_rotate-array/0189_rotate-array.cpp
+++ Array/0189_rotate-array/0189_rotate-array.cpp
@@ -16,11 +16,8 @@ public:
     }
     void rotate(vector<int>& nums, int k) {
         int n=nums.size();
-        for(int i=0;i<n/2;i++){
-            if((i>=0 && i<k)||(i>=n-k && i<n)){
-                swap(nums[i],nums[n-i-1]);
-            }
-        }
+        k=k%n;
+        reverseArray(nums,0,n-1);
         reverseArray(nums,0,k-1);
         reverseArray(nums,k,n-1);
     }
