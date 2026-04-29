f15705f134154e348554d5f4fc2421c03b225fab
Subhojit <ksubhojit20@gmail.com>
2026-04-29 09:26:54 +0530

    Add Medium solution: 71. Simplify Path [cpp]

---
+++ String/0071_simplify-path/0071_simplify-path.cpp
@@ -0,0 +1,51 @@
+/*
+ * Problem  : 71. Simplify Path
+ * Difficulty: Medium
+ * Topics   : String, Stack
+ * URL      : https://leetcode.com/problems/simplify-path/
+ */
+
+class Solution {
+public:
+    string simplifyPath(string path) {
+        stack<string> st;
+        int len=path.length();
+        int l=0;
+        while(l<len){
+            if(path[l]=='/'){
+                while(l<len && path[l]=='/'){
+                    l++;
+                }
+            }else if(path[l]=='.'){
+                string temp="";
+                while(l<len && path[l]=='.'){
+                    temp=temp+path[l];
+                    l++;
+                }
+                if(temp==".."){
+                    if(!st.empty())
+                    st.pop();
+                }else if(temp=="."){
+                    continue;
+                }else{
+                    st.push(temp);
+                }
+            }else{
+                string temp="";
+                while(l<len && path[l]!='/' && path[l]!='.'){
+                    temp=temp+path[l];
+                    l++;
+                }
+                st.push(temp);
+            }
+            
+        }
+        string ans="";
+        while(!st.empty()){
+            string t=st.top();
+            ans="/"+t+ans;
+            st.pop();
+        }
+        return ans==""?"/":ans;
+    }
+};
