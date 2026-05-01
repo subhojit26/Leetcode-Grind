06a802647c979a63462400bdc89198a8614b02c1
Subhojit <ksubhojit20@gmail.com>
2026-05-01 23:30:37 +0530

    Update Medium solution: 33. Search in Rotated Sorted Array [cpp]

--- Array/0033_search-in-rotated-sorted-array/0033_search-in-rotated-sorted-array.cpp
+++ Array/0033_search-in-rotated-sorted-array/0033_search-in-rotated-sorted-array.cpp
@@ -7,53 +7,45 @@
 
 class Solution {
 public:
-    int findMinIndex(vector<int> nums){
-        int n=nums.size();
-        int i=0;
-        int j=n-1;
-        while(i<j){
-            int mid=(i+j)/2;
-            if(nums[mid]<nums[j]){
-                j=mid;
-            }else{
-                i=mid+1;
-            }
-        }
-        return i;
-    }
-    int binarySearch(vector<int> nums, int target){
-        int n=nums.size();
-        int i=0;
-        int j=n-1;
-        while(i<=j){
-            int mid=(i+j)/2;
+    int binarySearch(vector<int> nums, int low, int high, int target){
+        while(low<=high){
+            int mid=(low+high)/2;
 
             if(nums[mid]==target){
                 return mid;
-            } else if(nums[mid]<target){
-                i=mid+1;
+            }else if(nums[mid]>target){
+                high=mid-1;
             }else{
-                j=mid-1;
+                low=mid+1;
             }
         }
         return -1;
     }
     int search(vector<int>& nums, int target) {
-        int ind=findMinIndex(nums);
+        int n=nums.size();
+        int low=0;
+        int high=n-1;
+        int ind;
+        while(low<=high){
+            int mid=(low+high)/2;
 
-        vector<int> firstPart(nums.begin(), nums.begin() + ind);
-        vector<int> secondPart(nums.begin() + ind, nums.end());
-
-        int p1=-1,p2=-1;
-        p1=binarySearch(firstPart,target);
-        p2=binarySearch(secondPart,target);
-
-        if(p1==-1 && p2==-1){
-            return -1;
-        }else if(p1!=-1){
-            return p1;
-        }else{
-            return p2+ind;
+            if(mid>0 && mid<n-1 && nums[mid]<nums[mid-1] && nums[mid]<nums[mid+1]){
+                ind=mid;
+                break;
+            }else if(nums[mid]>nums[n-1]){
+                low=mid+1;
+            }else{
+                high=mid-1;
+            }
         }
+        int res1=binarySearch(nums,ind,n-1,target);
+        int res2=binarySearch(nums,0,ind-1,target);
+        if(res1!=-1){
+            return res1;
+        }
+        if(res2!=-1){
+            return res2;
+        }
+        return -1;
     }
 };
