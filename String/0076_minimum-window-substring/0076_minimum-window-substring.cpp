7150ff4271842337839de54f4829c6c338e53379
Subhojit <ksubhojit20@gmail.com>
2026-05-03 15:00:22 +0530

    Update Hard solution: 76. Minimum Window Substring [cpp]

--- String/0076_minimum-window-substring/0076_minimum-window-substring.cpp
+++ String/0076_minimum-window-substring/0076_minimum-window-substring.cpp
@@ -8,38 +8,40 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        unordered_map<char, int> need;
-        unordered_map<char, int> window;
         int ls=s.length();
         int lt=t.length();
-        int count=0;
+        unordered_map<char, int> window;
+        unordered_map<char, int> need;
 
         for(int i=0;i<lt;i++){
             need[t[i]]++;
         }
         int required=need.size();
         int l=0;
-        int res=INT_MAX;
         int r=0;
-        int start = 0;
+        int res=INT_MAX;
+        int count=0;
+        int start=0;
+
         while(r<ls){
-            char c = s[r];
+            char c=s[r];
             if(need.count(c)){
                 window[c]++;
                 if(window[c]==need[c]){
                     count++;
                 }
             }
-            
+
             while(count==required){
                 if(r-l+1<res){
                     start=l;
                     res=r-l+1;
                 }
+
                 char lChar=s[l];
                 if(need.count(lChar)){
                     window[lChar]--;
-                    if(window[lChar] < need[lChar]){
+                    if(window[lChar]<need[lChar]){
                         count--;
                     }
                 }
@@ -47,7 +49,7 @@ public:
             }
             r++;
         }
-
-       return res == INT_MAX ? "" : s.substr(start, res);
+        
+        return res==INT_MAX?"":s.substr(start,res);
     }
 };
