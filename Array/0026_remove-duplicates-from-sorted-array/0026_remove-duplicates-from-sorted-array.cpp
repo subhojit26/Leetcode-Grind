73dca0f9748d0b650236a17ae9d76569f2f5d8a2
Subhojit <ksubhojit20@gmail.com>
2026-05-04 08:58:07 +0530

    Add Easy solution: 26. Remove Duplicates from Sorted Array [cpp]

---
+++ Array/0026_remove-duplicates-from-sorted-array/0026_remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,28 @@
+/*
+ * Problem  : 26. Remove Duplicates from Sorted Array
+ * Difficulty: Easy
+ * Topics   : Array, Two Pointers
+ * URL      : https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+ */
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int n=nums.size();
+        int l=0;
+        int r=1;
+        int k=1;
+        while(r<n){
+            if(nums[l]==nums[r]){
+                r++;
+            }
+            else {
+                l++;
+                k++;
+                nums[l]=nums[r];
+                r++;
+            }
+        }
+        return k;
+    }
+};
