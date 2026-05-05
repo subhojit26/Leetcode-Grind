bf568e3491ca0f33d7d0797aa625237e58366929
Subhojit <ksubhojit20@gmail.com>
2026-05-05 09:17:27 +0530

    Update Medium solution: 322. Coin Change [cpp]

--- Array/0322_coin-change/0322_coin-change.cpp
+++ Array/0322_coin-change/0322_coin-change.cpp
@@ -7,28 +7,31 @@
 
 class Solution {
 public:
-    int solver(int ind, int n, vector<int> coins, int amount){
+    int solver(int ind, int n, vector<int> coins, int amount, vector<vector<int>> &dp){
         if(amount==0){
             return 0;
         }
         if(ind==n){
             return 1e9;
         }
+        if(dp[ind][amount]!=-1){
+            return dp[ind][amount];
+        }
 
         int take=1e9;
         if(coins[ind]<=amount){
-            take=1+solver(ind,n,coins,amount-coins[ind]);
+            take=1+solver(ind,n,coins,amount-coins[ind],dp);
             cout<<take<<" ";
         }
-        int notTake=solver(ind+1,n,coins,amount);
-        //cout<<min(take,notTake)<<"\n";
-        return min(take,notTake);
+        int notTake=solver(ind+1,n,coins,amount,dp);
+        return dp[ind][amount]=min(take,notTake);
 
     }
     int coinChange(vector<int>& coins, int amount) {
         int c=0;
         int n=coins.size();
-        int res=solver(0,n,coins,amount);
+        vector<vector<int>> dp(n+1,vector<int> (amount+1,-1));
+        int res=solver(0,n,coins,amount,dp);
         if(res>=1e9){
             return -1;
         }
