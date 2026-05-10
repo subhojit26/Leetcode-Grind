52300343a0c0a3e3f981e51f364ffc158b2292ac
Subhojit <ksubhojit20@gmail.com>
2026-05-10 22:28:07 +0530

    Update Easy solution: 1. Two Sum [cpp]

--- Array/0001_two-sum/0001_two-sum.cpp
+++ Array/0001_two-sum/0001_two-sum.cpp
@@ -8,19 +8,17 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        map<int,int> mp;
-        vector<int> ans;
-        for(int i=0;i<nums.size();i++){
+        unordered_map<int, int> mp;
+        int n=nums.size();
+        vector<int>ans;
+        for(int i=0;i<n;i++){
             if(mp.find(target-nums[i])!=mp.end()){
                 ans.push_back(i);
                 ans.push_back(mp[target-nums[i]]);
             }
+
             mp[nums[i]]=i;
         }
         return ans;
     }
 };
-
-
-
-//
